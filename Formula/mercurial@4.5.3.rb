f604b8e98bed7255610f539dde063527515f1235
Thomas Steinhausen <steinhausen@heidenhain.de>
2018-06-12 14:41:41 +0200

    + mercurial 4.5.3 formula

---
+++ Formula/mercurial@4.5.3.rb
@@ -0,0 +1,63 @@
+# No head build supported; if you need head builds of Mercurial, do so outside
+# of Homebrew.
+class Mercurial < Formula
+  desc "Scalable distributed version control system"
+  homepage "https://mercurial-scm.org/"
+  url "https://mercurial-scm.org/release/mercurial-4.5.3.tar.gz"
+  sha256 "9c4cb257ef1e59e9aba693cc285a6136f7bb4e8300f598ff50ff8b6fc77808fc"
+
+  bottle do
+    sha256 "bfa7214141279e0221d81c03d2558ac3e0013bcdeb62e353b3fa65389e635950" => :high_sierra
+    sha256 "df975ebf08b4a7b8e6b0e3b6b0cbfa9bf2b2489e8eaeb480729c4b8668f457c2" => :sierra
+    sha256 "0d10b8609d08895a4042f93158a0169145413adfe4855bbb62a22a1716b0a3d7" => :el_capitan
+  end
+
+  depends_on "python@2"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
+
+    system "make", "PREFIX=#{prefix}", "install-bin"
+
+    # Install chg (see https://www.mercurial-scm.org/wiki/CHg)
+    cd "contrib/chg" do
+      system "make", "PREFIX=#{prefix}", "HGPATH=#{bin}/hg", \
+             "HG=#{bin}/hg"
+      bin.install "chg"
+    end
+
+    # Configure a nicer default pager
+    (buildpath/"hgrc").write <<~EOS
+      [pager]
+      pager = less -FRX
+    EOS
+
+    (etc/"mercurial").install "hgrc"
+
+    # Install man pages, which come pre-built in source releases
+    man1.install "doc/hg.1"
+    man5.install "doc/hgignore.5", "doc/hgrc.5"
+
+    # install the completion scripts
+    bash_completion.install "contrib/bash_completion" => "hg-completion.bash"
+    zsh_completion.install "contrib/zsh_completion" => "_hg"
+  end
+
+  def caveats
+    return unless (opt_bin/"hg").exist?
+    cacerts_configured = `#{opt_bin}/hg config web.cacerts`.strip
+    return if cacerts_configured.empty?
+    <<~EOS
+      Homebrew has detected that Mercurial is configured to use a certificate
+      bundle file as its trust store for TLS connections instead of using the
+      default OpenSSL store. If you have trouble connecting to remote
+      repositories, consider unsetting the `web.cacerts` property. You can
+      determine where the property is being set by running:
+        hg config --debug web.cacerts
+    EOS
+  end
+
+  test do
+    system "#{bin}/hg", "init"
+  end
+end
